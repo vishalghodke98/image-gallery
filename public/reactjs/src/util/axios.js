25da09a42d49d1cc298c4962438ea40785d81400
vishalghodke98 <vishalghodke@kudosfinance.in>
2020-10-19 20:00:49 +0530

    axios changes

--- public/reactjs/src/util/axios.js
+++ public/reactjs/src/util/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    // baseURL: 'https://test.shopkart.club/'
-    baseURL: 'http://localhost:5000/'
+    baseURL: 'https://test.shopkart.club/'
+    // baseURL: 'http://localhost:5000/'
 });
 
 export default instance;
